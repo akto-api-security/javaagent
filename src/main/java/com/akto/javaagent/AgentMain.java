3046d3f070b4392a63949b528178787a26224340
Ankush Jain <ankush@akto.io>
2022-02-07 03:41:07 +0530

    complete implementation of ApacheHttpClient

--- src/main/java/com/akto/javaagent/AgentMain.java
+++ src/main/java/com/akto/javaagent/AgentMain.java
@@ -17,4 +17,16 @@ public class AgentMain {
         .with(AgentBuilder.TypeStrategy.Default.REDEFINE)
         .installOn(inst);
   }
+  public static void agentmain(final String agentArgs,
+                 final Instrumentation inst) throws Exception {
+    System.out.printf("Starting %s\n", AgentMain.class.getSimpleName());
+
+    new AgentBuilder.Default()
+        .type(new HttpClientMatcher())
+        .transform(new HttpClientMatcher())
+        .type(new OkHttpClientMatcher())
+        .transform(new OkHttpClientMatcher())
+        .with(AgentBuilder.TypeStrategy.Default.REDEFINE)
+        .installOn(inst);
+  }
 }
--- src/main/java/com/akto/javaagent/HttpClientMatcher.java
+++ src/main/java/com/akto/javaagent/HttpClientMatcher.java
@@ -2,7 +2,6 @@ package com.akto.javaagent;
 
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.asm.AsmVisitorWrapper;
-import net.bytebuddy.description.type.TypeDefinition;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.matcher.ElementMatcher;
@@ -10,53 +9,31 @@ import net.bytebuddy.matcher.ElementMatchers;
 import net.bytebuddy.utility.JavaModule;
 import net.bytebuddy.agent.builder.AgentBuilder.Transformer;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.mongodb.BasicDBObject;
 
+import org.apache.http.Header;
+import org.apache.http.HeaderElement;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
+import org.apache.http.RequestLine;
+import org.apache.http.StatusLine;
+import org.apache.http.util.ByteArrayBuffer;
 
 public class HttpClientMatcher extends ElementMatcher.Junction.AbstractBase<TypeDescription> implements Transformer {
-    private static ElementMatcher<TypeDescription> elem = ElementMatchers.named("org.apache.http.client.HttpClient");
+    private static ElementMatcher<TypeDescription> elem = ElementMatchers.isSubTypeOf(org.apache.http.client.HttpClient.class);
 
-    private boolean hasInterface(
-      TypeDefinition typeDefinition, Set<TypeDescription> checkedInterfaces) {
-        for (TypeDefinition interfaceType : typeDefinition.getInterfaces()) {
-          TypeDescription erasure = interfaceType.asErasure();
-          if (erasure != null) {
-            if (debug) {
-              System.out.println("check: " + interfaceType.asGenericType().asErasure() + " " + elem.matches(interfaceType.asGenericType().asErasure()));
-            }
-  
-            if (checkedInterfaces.add(interfaceType.asErasure())
-                && (elem.matches(interfaceType.asGenericType().asErasure())
-                    || hasInterface(interfaceType, checkedInterfaces))) {
-              return true;
-            }
-          }
-        }
-        return false;
-      }
+    public static final List<BasicDBObject> apiCalls = new ArrayList<>();
 
-    public boolean matches(TypeDescription target, boolean interfacesOnly) {
-      Set<TypeDescription> checkedInterfaces = new HashSet<>(8);
-      TypeDefinition typeDefinition = target;
-      while (typeDefinition != null) {
-        if (debug) {
-          System.out.println("typedef: " + typeDefinition + elem.matches(typeDefinition.asGenericType().asErasure()));
-        }
-        if (((!interfacesOnly || typeDefinition.isInterface())
-                && elem.matches(typeDefinition.asGenericType().asErasure()))
-            || hasInterface(typeDefinition, checkedInterfaces)) {
-          return true;
-        }
-        typeDefinition = typeDefinition.getSuperClass();
-      }
-      return false;
-    }
-  
     boolean debug = false;
+    public static final int MAX_ENTITY_LEN = 10_000_000;
 
     @Override
     public DynamicType.Builder<?> transform(
@@ -65,7 +42,6 @@ public class HttpClientMatcher extends ElementMatcher.Junction.AbstractBase<Type
         final ClassLoader classLoader,
         final JavaModule module) {
 
-        System.out.println(typeDescription);  
         debug = true;
         
         final AsmVisitorWrapper methodsVisitor =
@@ -74,32 +50,132 @@ public class HttpClientMatcher extends ElementMatcher.Junction.AbstractBase<Type
               ElementMatchers.named("execute")
               .and(ElementMatchers.isMethod())
               .and(ElementMatchers.not(ElementMatchers.isAbstract()))
-            . and(ElementMatchers.takesArgument(1, ElementMatchers.named("org.apache.http.HttpRequest")))
+            . and(ElementMatchers.takesArgument(0, ElementMatchers.isSubTypeOf(org.apache.http.HttpRequest.class)))
             );
         return builder.visit(methodsVisitor);
     }
 
+    public static String toJsonStr(Header[] headers) {
+      BasicDBObject ret = new BasicDBObject();
+
+      if (headers == null) {
+        return ret.toJson();
+      }
+
+      for (Header header: headers) {
+        ret.put(header.getName(), header.getValue());
+      }
+
+      return ret.toJson();
+
+    }
+
+    public static void addConstants(BasicDBObject obj) {
+      obj.put("ip", "");
+      obj.put("time", ""+(int)(System.currentTimeMillis()/1000l));
+      obj.put("akto_account_id", "" + 1_000_000);
+      obj.put("akto_vxlan_id", 123);
+      obj.put("source", "OTHER");
+    }
+
+    public static String readEntity(HttpEntity entity) {
+      String ret = "";
+
+      if (entity == null || entity.getContentLength() == 0 || !entity.isRepeatable()) {
+        return ret;
+      }
+
+      try {
+          final InputStream instream = entity.getContent();
+          if (instream == null || entity.getContentLength() > MAX_ENTITY_LEN) {
+              return ret;
+          }
+
+          int i = (int)entity.getContentLength();
+          if (i < 0) {
+              i = 4096;
+          }
+          final ByteArrayBuffer buffer = new ByteArrayBuffer(i);
+          final byte[] tmp = new byte[4096];
+          int l;
+          while((l = instream.read(tmp)) != -1) {
+              buffer.append(tmp, 0, l);
+          }
+          return new String(buffer.toByteArray(), StandardCharsets.ISO_8859_1);
+      } catch (IOException e) {
+
+      }
+      
+      return ret;
+    }
+
 
     private static class EnterAdvice {
+
+
       @Advice.OnMethodEnter(suppress = Throwable.class)
-      public static void enter(@Advice.Argument(1) HttpRequest request) {
-        System.out.println(request.getRequestLine().toString());
+      public static BasicDBObject enter(@Advice.AllArguments Object[] args) {
+        BasicDBObject ret = new BasicDBObject();
+        try { 
+          for(Object object: args) {
+
+            if (object instanceof HttpRequest) {
+              HttpRequest request = (HttpRequest) object;
+              RequestLine requestLine = request.getRequestLine();
+
+              ret.put("path", requestLine.getUri());
+              ret.put("method", requestLine.getMethod());
+              ret.put("type", requestLine.getProtocolVersion().toString());
+              ret.put("requestHeaders", toJsonStr(request.getAllHeaders()));
+
+              String requestPayload = "";
+
+              if (request instanceof HttpEntityEnclosingRequest) {
+                HttpEntityEnclosingRequest enclosingRequest = (HttpEntityEnclosingRequest) request;
+                HttpEntity entity = enclosingRequest.getEntity();
+                requestPayload = readEntity(entity);
+              }
+
+              ret.put("requestPayload", requestPayload);
+            }
+          }
+        } catch (Exception e) {
+
+        }
+        return ret;
+        
       }
     }
 
     private static class ExitAdviceMethods {
       @Advice.OnMethodExit(suppress = Throwable.class)
-      public static void exit(@Advice.Return Object response) {
-        if (response instanceof HttpResponse) {
-          HttpResponse httpResponse = (HttpResponse) response;
-          System.out.println(httpResponse.getStatusLine());
-          
+      public static void exit(@Advice.Return Object responseObj, @Advice.Enter BasicDBObject returnFromEnter) {
+
+        if (returnFromEnter == null) {
+          return;
         }
+
+        if (!(responseObj instanceof HttpResponse)) {
+          return;
+        }
+
+        HttpResponse response = (HttpResponse) responseObj;
+
+        StatusLine statusLine = response.getStatusLine();
+        returnFromEnter.put("statusCode", statusLine.getStatusCode()+"");
+        returnFromEnter.put("responseHeaders", toJsonStr(response.getAllHeaders()));
+        returnFromEnter.put("status", statusLine.getReasonPhrase());
+        HttpEntity entity = response.getEntity();
+        returnFromEnter.put("responsePayload", readEntity(entity));        
+
+        addConstants(returnFromEnter);
+
+        apiCalls.add(returnFromEnter);
       }  
     }
 
     @Override
     public boolean matches(TypeDescription target) {
-      return this.matches(target, true);
+      return elem.matches(target);
     }    
 }
